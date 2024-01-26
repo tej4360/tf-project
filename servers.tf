35641ef464efd99ac9ed30c73a0ac9e33186b1ad
raviteja <teja4360@gmail.com>
2024-01-26 08:39:07 +0530

    Random txt

--- servers.tf
+++ servers.tf
@@ -1,31 +1,32 @@
-module "app_servers" {
-  depends_on = [module.db_servers]
-  for_each = var.app_servers
-  source = "./common"
-  instance_type = each.value["type"]
-  component_name = each.value["name"]
-  password = lookup(each.value, "password", "null" )
-  app_type = "app"
-  env=var.env
-}
-
-module "db_servers" {
-  for_each = var.db_servers
-  source = "./common"
-  instance_type = each.value["type"]
-  component_name = each.value["name"]
-  password = lookup(each.value, "password", "null")
-  app_type = "db"
-  env= var.env
-#  provisioner = true
-}
+#module "app_servers" {
+#
+#  depends_on = [module.db_servers]
+#  for_each = var.app_servers
+#  source = "./common"
+#  instance_type = each.value["type"]
+#  component_name = each.value["name"]
+#  password = lookup(each.value, "password", "null" )
+#  app_type = "app"
+#  env=var.env
+#}
+#
+#module "db_servers" {
+#  for_each = var.db_servers
+#  source = "./common"
+#  instance_type = each.value["type"]
+#  component_name = each.value["name"]
+#  password = lookup(each.value, "password", "null")
+#  app_type = "db"
+#  env= var.env
+##  provisioner = true
+#}
 
 module "eks" {
   source             = "git::https://github.com/tej4360/tf-eks.git"
   ENV                = var.env
   eks_version        = 1.27
-  PRIVATE_SUBNET_IDS = var.default_subnet
-  PUBLIC_SUBNET_IDS  = var.default_subnet
+#  PRIVATE_SUBNET_IDS = var.default_subnet
+#  PUBLIC_SUBNET_IDS  = var.default_subnet
   DESIRED_SIZE       = 2
   MAX_SIZE           = 2
   MIN_SIZE           = 2
